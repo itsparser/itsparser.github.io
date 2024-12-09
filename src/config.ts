dc0ea6a2b4a8a37bdb8a68afe1488215c6726c47
Vasanth Kumar <itsparser@gmail.com>
2024-12-09 14:31:57 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://vasanth.github.io", // replace this with your deployed domain
   author: "Vasanth",
   profile: "https://Vasanth.dev/",
   desc: "A minimal, responsive and SEO-friendly portfolio.",
